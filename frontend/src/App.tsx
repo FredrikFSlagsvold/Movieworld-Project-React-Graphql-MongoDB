08e9f597d4d5620b74635d70c82817ea72e3eb63
Marianne Bjørseth Stålen <98464130+maribsta@users.noreply.github.com>
2022-10-14 14:09:48 +0200

    connect to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, useQuery, gql} from '@apollo/client';
 
-
-const client = new ApolloClient({
-  uri: 'https://it2810-20.idi.ntnu.no:27017',
-  cache: new InMemoryCache(),
-});
-
-client
-  .query({
-    query: gql`
-      query  {
-        movies {
-          id
-          firstname
-          lastname
-        
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
 
 function App() {
+
+  const getUsers = gql`
+  query Query {
+  users{
+  id
+  }
+  }
+ `; 
+
+  const data = useQuery(getUsers)
+
+
+console.log("hei", data)
+
   return (
     <div>
 hei
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { ApolloClient, InMemoryCache, useQuery, gql, ApolloProvider} from '@apollo/client';
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache(),
+});
+
+// const data = useQuery(
+//     gql`
+//       query  {
+//         users {
+//           id
+//           firstname
+//           lastname
+//         }
+//       }
+//     `
+// )
+
+// console.log(data)
+
+
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
