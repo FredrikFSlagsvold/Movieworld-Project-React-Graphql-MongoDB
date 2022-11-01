0f5700ade96e1f560bf8c35df824af250ee0aa85
= <=>
2022-11-01 10:42:05 +0100

    not finished styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,27 @@
+import { Button, ButtonGroup } from "@mui/material";
 import { Link } from "react-router-dom";
 import "./header.css"
 
 
 export default function Header(){
+  function logout(){
+    sessionStorage.setItem("isLoggedIn", "false")
+    window.location.reload()
+  }
+
+
     return (
-        <div className="href" style={{justifyContent: ''}}>
-          <div><p id='logoName'>M O V I E W O R L D</p></div>
-          <div className="link"><Link to="/">HomePage</Link></div>
-            {/* <div className="link"><Link to="/search">Search</Link></div> */}
-            <div className="link"><Link to="/displayUsers">Display Users</Link></div>
-            <div className="link"><Link to="/movieoffset">Movie offset</Link></div>
-            <div className="link"><Link to="login">Login</Link></div>
-        </div>
+      <div className="href">
+      <div className="link"><Link data-testid="HomePageLink" to="/">M O V I E W O R L D</Link></div>
+      <div className="topRight">
+        <ButtonGroup variant="text" color="inherit" aria-label="text button group">
+          <Button >Liked movies</Button>
+          {sessionStorage.getItem("isLoggedIn") === "true" ?  <Button onClick={logout}>Logout</Button> : <div className="link"><Link to="/login">Login</Link></div>}
+        </ButtonGroup>
+        <div className="link"><Link data-testid="LikedMoviesLink" reloadDocument={true} to="/liked">Liked Movies</Link></div>
+      </div>
+    </div>
+
       );
 }
 
