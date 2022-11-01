3c761991094d907d97df00d9b43986edca876ee2
= <=>
2022-11-01 09:08:27 +0100

    feat: merge fixes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,22 +1,17 @@
 import { Link } from "react-router-dom";
 import "./header.css"
-import { Button } from "@mui/material";
 
 
 export default function Header(){
-    function logout(){
-     sessionStorage.setItem("isLoggedIn", "false")
-     window.location.reload();
-
-  }
-
-  
     return (
-        <div className="href">
-          <div className="link"><Link data-testid="HomePageLink" to="/">HomePage</Link></div>
-            <div className="link"><Link data-testid="LikedMoviesLink" reloadDocument={true} to="/liked">Liked Movies</Link></div>
-            
-            {sessionStorage.getItem("isLoggedIn") === "true" ?  <Button onClick={logout}  variant="contained">Logout</Button> : <div className="link"><Link to="/login">Login</Link></div>}
+        <div className="href" style={{justifyContent: ''}}>
+          <div><p id='logoName'>MOVIEWORLD</p></div>
+          <div className="link"><Link to="/">HomePage</Link></div>
+            {/* <div className="link"><Link to="/search">Search</Link></div> */}
+            <div className="link"><Link to="/displayUsers">Display Users</Link></div>
+            <div className="link"><Link to="/movieoffset">Movie offset</Link></div>
+            <div className="link"><Link to="login">Login</Link></div>
+            <a href="login" target="_blank">Login</a>
         </div>
       );
 }
