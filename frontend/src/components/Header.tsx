db7446eb88896e75307ec88ef45dba7ac6c46e60
= <=>
2022-11-01 09:34:46 +0100

    styling#

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,13 +5,12 @@ import "./header.css"
 export default function Header(){
     return (
         <div className="href" style={{justifyContent: ''}}>
-          <div><p id='logoName'>MOVIEWORLD</p></div>
+          <div><p id='logoName'>M O V I E W O R L D</p></div>
           <div className="link"><Link to="/">HomePage</Link></div>
             {/* <div className="link"><Link to="/search">Search</Link></div> */}
             <div className="link"><Link to="/displayUsers">Display Users</Link></div>
             <div className="link"><Link to="/movieoffset">Movie offset</Link></div>
             <div className="link"><Link to="login">Login</Link></div>
-            <a href="login" target="_blank">Login</a>
         </div>
       );
 }
